7b5430fce09fb3f3a92adc583b9e3d3481fe6495
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-04-04 20:29:57 +0400

    main.tf has been updated with the firewall rules

--- resources/main.tf
+++ resources/main.tf
@@ -1,6 +1,52 @@
 resource "digitalocean_droplet" "foobar" {
-  name   = "example"
+  name   = "splunk"
   size   = "s-1vcpu-1gb"
   image  = "ubuntu-22-04-x64"
   region = "nyc3"
 }
+
+resource "digitalocean_firewall" "splunk" {
+  name = "only-22-80-and-443"
+
+  droplet_ids = [digitalocean_droplet.splunk.id]
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
