33fc9ee308b81155835319ea22ea89b475cfe3cf
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-04-04 21:50:25 +0400

    main.tf updated

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "splunk" {
+  source = "./modules/splunk"
+}
+
+module "nginx" {
+  source = "./modules/nginx"
+}
+
+module "firewall" {
+  source   = "./modules/firewall"
+  tag_name = "splunk-dev"
+}
