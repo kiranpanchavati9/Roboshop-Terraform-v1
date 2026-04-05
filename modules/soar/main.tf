c3402cde040933cc64709beeed74610cdb148369
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-04-05 10:57:29 +0400

    soar module has been updated

---
+++ modules/soar/main.tf
@@ -0,0 +1,13 @@
+# Create a new tag
+resource "digitalocean_tag" "soar-prod" {
+  name = "soarprod"
+}
+
+# Create a new Droplet in nyc3 with the foobar tag
+resource "digitalocean_droplet" "soar-prod" {
+  image  = "ubuntu-18-04-x64"
+  name   = "fortisoar-prod"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  tags   = [digitalocean_tag.soar-prod.name]
+}
