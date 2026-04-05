be027fff98908e7d2bb1f574cef63a0651d65604
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-04-05 11:43:41 +0400

    tags has been created and updated

--- modules/soar/main.tf
+++ modules/soar/main.tf
@@ -1,6 +1,6 @@
 # Create a new tag
 resource "digitalocean_tag" "soar_prod_tag" {
-  name = [var.tag_name]
+  name = var.tag_name
 }
 
 # Create a new Droplet in nyc3 with the foobar tag
@@ -9,5 +9,5 @@ resource "digitalocean_droplet" "soar-prod" {
   name   = "fortisoar-prod"
   region = "nyc3"
   size   = "s-1vcpu-1gb"
-  tags = [var.tag_name]
+  tags = [digitalocean_tag.soar_prod_tag.name]
 }
