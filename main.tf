8ff1791f4aefb0e739bea794c72806f3aed59551
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-04-05 12:48:29 +0400

    user droplet has been created

---
+++ main.tf
@@ -0,0 +1,33 @@
+# Create a new tag for the user
+resource "digitalocean_tag" "user" {
+  name = "user"
+}
+
+# Create a new Droplet in nyc3 with the user tag
+resource "digitalocean_droplet" "user" {
+  image  = "ubuntu-22-04-x64"
+  name   = "web-1"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  tags   = [digitalocean_tag.user.name]
+}
+
+## FIREWALL RULE ALLOW ALL FOR THE USER
+
+resource "digitalocean_firewall" "roboshop-firewall-all" {
+  name = "allow-all-roboshop"
+
+  tags = [digitalocean_tag.user.name]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "0-65535"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "0-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
